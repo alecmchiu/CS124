69bbb81155da62584d463e1626a3f34d67393983
Alec Chiu <alecmchiu@gmail.com>
2016-05-23 01:45:16 -0700

    Finish hole generator

--- Imputation/hole_generator.R
+++ Imputation/hole_generator.R
@@ -2,17 +2,57 @@
 
 setwd('/Users/Alec/documents/school/ucla/spring 2016/cs 124/programs/imputation')
 
-full_data <- read.table('SNP_Status.txt',header = TRUE)
+rows_wanted = 1000
+columns_wanted = 100
+percent = 0.1
+title = 'real_1000SNP_n100'
+del_type = 'cell' #cell, col, or row
 
-rows <- nrow(full_data)
-columns <- ncol(full_data)
+set <- 2
+full_data_sets <- c('SNP_Status.txt','imputation_training.txt')
 
-subset_data <- full_data[,1:100]
-write.table(subset_data, file = "title.txt")
-# full_data[columns] <- NULL
-# 
-# num_dels <- 0.1*rows*columns
-# 
-# for (i in seq_len(num_dels)){
-#     full_data[round(runif(1,1,rows)),round(runif(1,1,columns))] <- NA
-# }
+full_data <- read.table(full_data_sets[set],header = TRUE)
+
+subset_data <- full_data[1:rows_wanted,1:columns_wanted]
+write.table(subset_data, file = paste(title,"_key.txt",sep=''))
+
+rows <- nrow(subset_data)
+columns <- ncol(subset_data)
+
+#full_data[columns] <- NULL #only for PA1
+
+#cell_dels
+if (del_type == 'cell'){
+    num_dels <- percent*rows*columns
+    
+    for (i in seq_len(num_dels)){
+        subset_data[round(runif(1,1,rows)),round(runif(1,1,columns))] <- NA
+    }
+    
+    sum(is.na(subset_data))
+    
+    write.table(subset_data,paste(title,'.txt',sep=''))
+}
+
+#column_dels
+if (del_type = 'col'){
+    col_dels <- percent*columns
+    for (i in seq_len(col_dels)){
+        subset_data[,round(runif(1,1,columns))] <- NA
+    }
+    sum(is.na(subset_data))
+    
+    write.table(subset_data,paste(title,'.txt',sep=''))
+}
+
+#row_dels
+if (del_type == 'row'){
+    row_dels <- percent*rows
+    for (i in seq_len(row_dels)){
+        subset_data[round(runif(1,1,rows)),] <- NA
+    }
+    
+    sum(is.na(subset_data))
+    
+    write.table(subset_data,paste(title,'.txt',sep=''))
+}
--- Imputation/imputation.R
+++ Imputation/imputation.R
@@ -6,9 +6,12 @@ set <- 3
 
 training <- c('PA1_train.txt','PA1_train.txt','imputation_training.txt')
 testing <- c('PA1_test_pure_random.txt','PA1_test_sys_random.txt','imputation_test.txt')
+key_sets <- c('PA1_test_key.txt','PA1_test_key.txt')
 
-train <- read.table(training[set],header = TRUE)
-test <- read.table(testing[set],header = TRUE, fill = TRUE)
+credit_set <- length(training)
+
+train <- read.table(training[set], header = TRUE)
+test <- read.table(testing[set], header = TRUE, fill = TRUE)
 
 means <- round(colMeans(train))
 header <- colnames(test)
@@ -22,9 +25,8 @@ for (i in seq_len(nrow(test))){
         }
     }
 }
-if (set == 1 || set == 2){
-    key <- read.table('PA1_test_key.txt')
-    
+if (set != credit_set){
+    key <- read.table(key_sets[set], header = TRUE)
     error <- 0
     for (i in seq_len(nrow(test))){
         for (j in seq_len(ncol(test))){
