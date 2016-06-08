c627ba2a512b6649626a0857530bedf3d48023c0
Alec Chiu <alecmchiu@gmail.com>
2016-06-07 19:15:43 -0700

    Basic similarity approach complete

--- Imputation/regression.R
+++ Imputation/regression.R
@@ -3,7 +3,7 @@ start <- proc.time()
 setwd('/Users/Alec/documents/school/ucla/spring 2016/cs 124/programs/imputation')
 
 set <- 1 # 1=100 SNP, 2=1000SNP, 3=10000SNP
-type <- 2 # 1=random, 2 = systematic
+type <- 1 # 1=random, 2 = systematic
 
 if (type == 1){
     data_set <- c('100SNP_R.txt','1000SNP_R.txt','10000SNP_R.txt')
@@ -18,6 +18,9 @@ key <- read.table(key[set], header = TRUE)
 holes <- sum(is.na(test))
 empty <- which(is.na(test),TRUE)
 
+#cor_mat <- cor(t(test),use='pairwise.complete.obs')
+#diag(cor_mat) <- 0
+
 NA_cols <- names(test[colSums(is.na(test)) > 0])
 if (type == 2){
     inference_cols <- names(test[colSums(is.na(test)) == 0])
@@ -26,33 +29,8 @@ if (type == 2){
 }
 test[is.na(test)] <- -1 #set all NA to -1 for easier handling
 
-for (column in NA_cols){
-    similarity_score <- 0
-    best_column <- NA
-    for (each in inference_cols){
-        if (column == each){
-            next
-        } else {
-            score <- sum(test[,column]==test[,each])
-            if (score > similarity_score){
-                similarity_score <- score
-                best_column <- each
-            }
-        }
-    }
-    NA_vals <- which(test[,column]==-1)
-    for (i in seq_len(length(NA_vals))){
-        test[,column][NA_vals[i]] <- test[,best_column][NA_vals[i]]
-    }
-}
-#sum(test==-1)
-if (sum(test == -1) > 0){
-    means <- round(rowMeans(test, na.rm = TRUE))
-    leftover <- which(test==-1,TRUE)
-    for (i in seq_len(nrow(leftover))){
-        test[leftover[i,1],leftover[i,2]] <- means[leftover[i,1]]
-    }
-}
+
+
 run_time <- proc.time() - start
 
 error <- 0
